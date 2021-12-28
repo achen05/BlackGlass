b4b29819ab2480ce842bc544f717cc99c5e2ab64
achen05 <72044476+achen05@users.noreply.github.com>
2021-12-28 15:30:48 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+let img;
+function preload() {
+  img = loadImage('glass.svg');
+}
+
+function setup() {
+  var canvas = createCanvas(200, 200, WEBGL);
+  canvas.parent("glass-box");
+
+}
+
+function draw() {
+  background(0);
+  rotateZ(frameCount * 0.005);
+  rotateX(frameCount * 0.005);
+  rotateY(frameCount * 0.005);
+  //pass image as texture
+  texture(img);
+  box(width / 2);
+  strokeWeight(0);
+}
